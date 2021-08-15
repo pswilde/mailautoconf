1df0b80c1f02045373f064c4534081fd2a45beca
Paul Wilde <paul@wilde.cloud>
2021-08-15 20:57:30 +0100

    created gitversion branch

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+  "net/http"
+  "log"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+  http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func do_this(){
+  fmt.Println("doing this")
+}
